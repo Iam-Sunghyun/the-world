a511ccbd04bfff7abfb23da43813561f08ed0e50
전성현 <sunghyun1148@gmail.com>
2023-11-08 23:10:49 +0900

    네비게이션바 추가

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,18 +1,30 @@
 import { NavLink } from 'react-router-dom';
-import test from './test.module.css';
+import style from './NavigationBar.module.css';
+
+// const isActive = ({ isActive, isPending, isTransitioning }) => {
+//   return {
+//     fontWeight: isActive ? 'bold' : '',
+//     color: isPending ? 'red' : 'black',
+//     viewTransitionName: isTransitioning ? 'slide' : '',
+//   };
+// };
 
 function NavigationBar() {
   return (
-    <nav>
+    <nav className={style.nav}>
+      <div className={style['nav-icon-container']}>
+        <img className={style['nav-icon']} src='public\free-icon-earth-globe-1719481.png' alt='' />
+        <NavLink to='/' className={style['nav-home-text']}>The World</NavLink>
+      </div>
       <ul>
         <li>
-          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/products'>PRODUCTS</NavLink>
         </li>
         <li>
-          <NavLink to='/pricing'>Pricing</NavLink>
+          <NavLink to='/pricing'>PRICING</NavLink>
         </li>
         <li>
-          <NavLink to='/products'>Products</NavLink>
+          <NavLink to='/products'>LOGIN</NavLink>
         </li>
       </ul>
     </nav>
