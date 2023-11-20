93995913bb7147208b88b7c98d5d93780d383690
전성현 <sunghyun1148@gmail.com>
2023-11-20 22:59:47 +0900

    라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
-import Product from './pages/Products';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-        {/* path="요청 경로", element="컴포넌트가 반환하는 리액트 엘리먼트"   */}
-        {/* 요청시 특정 path에 대응되는 컴포넌트가 화면에 렌더링되고 나머지는 무시된다*/}
-        <Route path='/' element={<Homepage />} />
-        <Route path='/products' element={<Product />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavigationBar />
+          <Routes>
+            {/* path="요청 경로", element="컴포넌트가 반환하는 리액트 엘리먼트"   */}
+            {/* 요청시 특정 path에 대응되는 컴포넌트가 화면에 렌더링되고 나머지는 무시된다*/}
+            <Route path='/' element={<Homepage />} />
+            <Route path='/products' element={<Product />} />
+            <Route path='/pricing' element={<Pricing />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/app' element={<AppLayout />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
