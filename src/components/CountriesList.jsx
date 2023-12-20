72bf3a9a20b4670ac280f1794ade0c62c1a04453
전성현 <sunghyun1148@gmail.com>
2023-12-20 23:47:58 +0900

    국가 리스트 컴포넌트 생성

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,42 @@
+import styles from './CountriesList.module.css';
+import CountryItem from './CountryItem';
+import Spinner from './Spinner';
+
+function CountriesList({ cities, isLoading }) {
+  // const [countries, setCountires] = useState(new Set());
+
+  // 로딩 표시용 회전 효과
+  if (isLoading) return <Spinner />;
+
+  // cities 데이터가 없는 경우 표시용
+  if (cities.length === 0) return '국가 정보가 없습니다.';
+
+  // cities props에서 중복되지 않는 국가 리스트 생성
+  const countries = cities.reduce((acc, city) => {
+    if (!acc.map((el) => el.country).includes(city.country)) {
+      return [
+        ...acc,
+        {
+          country: city.country,
+          emoji: city.emoji,
+          id: city.id,
+          position: {
+            lat: city.position.lat,
+            lng: city.position.lng,
+          },
+        },
+      ];
+    }
+    return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countriesList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
