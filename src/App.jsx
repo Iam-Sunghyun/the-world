e9d0ba0ca98d759bbbf992047bd9d89a8ce99127
전성현 <sunghyun1148@gmail.com>
2023-12-21 22:14:59 +0900

    라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import City from './components/City';
 import CityList from './components/CityList';
-import Countries from './components/Countries';
+import Countries from './components/CountriesList';
 import Form from './components/Form';
 import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
@@ -13,22 +14,21 @@ import Product from './pages/Product';
 const FAKE_SERVER_BASE_URL = 'http://localhost:8000';
 
 function App() {
-  const [cities, setCities] = useState({});
+  const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   // fake json 데이터 fetch
   useEffect(() => {
     async function fetchCities() {
+      setIsLoading(true);
       try {
-        setIsLoading(true);
         const fetched = await fetch(`${FAKE_SERVER_BASE_URL}/cities`);
         const data = await fetched.json();
         setCities(data);
       } catch (e) {
         console.log(e);
-      } finally {
-        setIsLoading(false);
       }
+      setIsLoading(false);
     }
     fetchCities();
   }, []);
@@ -44,9 +44,10 @@ function App() {
           <Route path='/pricing' element={<Pricing />} />
           <Route path='/login' element={<Login />} />
           <Route path='/app' element={<AppLayout />}>
-            <Route index element={<CityList />} />
-            <Route path='cities' element={<CityList />} />
-            <Route path='countries' element={<Countries />} />
+            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route path='cities/:id' element={<City />} />
+            <Route path='countries' element={<Countries cities={cities} isLoading={isLoading} />} />
             <Route path='form' element={<Form />} />
           </Route>
           <Route path='*' element={<PageNotFound />} />
