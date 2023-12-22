4acbdc35bc02572e6dfd7d5c639ddcb333ac1873
전성현 <sunghyun1148@gmail.com>
2023-12-22 23:49:59 +0900

    Navigate, Countries 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import City from './components/City';
 import CityList from './components/CityList';
 import Countries from './components/CountriesList';
@@ -44,10 +44,10 @@ function App() {
           <Route path='/pricing' element={<Pricing />} />
           <Route path='/login' element={<Login />} />
           <Route path='/app' element={<AppLayout />}>
-            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route index element={<Navigate test="test" to='cities' /> } />
             <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
             <Route path='cities/:id' element={<City />} />
-            <Route path='countries' element={<Countries cities={cities} isLoading={isLoading} />} />
+            <Route path='countries' element={ <Countries cities={cities} isLoading={isLoading} />} />
             <Route path='form' element={<Form />} />
           </Route>
           <Route path='*' element={<PageNotFound />} />
