b91b30e97f86a4ab0ac129b42a1ed7f4919ae41d
전성현 <sunghyun1148@gmail.com>
2024-01-09 23:13:42 +0900

    leaflet 지도 추가

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,60 @@
-import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
+import { useNavigate } from 'react-router-dom';
+import { useCities } from '../contexts/CitiesContext';
 import styles from './Map.module.css';
 
 function Map() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
+  const { cities, currentCity } = useCities();
 
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
-  const { id } = useParams();
-
-  // console.log(searchParams);
   return (
-    <div className={styles.map} onClick={() => navigate('form')}>
-      Position: {lat}, {lng}. {id}
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={[currentCity.position?.lat || 37, currentCity.position?.lng || 127]}
+        zoom={6}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+        />
+        {/* 지도 마커 */}
+        {cities.map(({ id, position, cityName, emoji, notes }) => (
+          <Marker key={id} position={[position.lat, position.lng]}>
+            <Popup>
+              <span>{emoji}</span>
+              {cityName}
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeMarker
+          position={[currentCity.position?.lat || 37, currentCity.position?.lng || 127]}
+        />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+}
+
+// 도시 항목 클릭시 위치 이동 커스텀훅
+function ChangeMarker({ position }) {
+  const map = useMap(); // leaflet Map 객체 반환
+  map.setView(position); // 지도 위치 설정
+  return null;
+}
+
+// Map 객체에 이벤트 핸들러 등록을 위한 커스텀훅
+function DetectClick() {
+  const navigate = useNavigate();
+
+  // Map 객체에 이벤트 핸들러 등록
+  useMapEvents({
+    click: (e) => {
+      // form으로 이동하면서 이벤트 객체(LeafletEventHandlerFnMap)의 클릭 위치 값을 쿼리스트링으로 전달 
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`);
+    },
+  });
 }
 
 export default Map;
